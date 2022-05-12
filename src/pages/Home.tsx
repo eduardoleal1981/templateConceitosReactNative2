cddfa1196d432b58dd6ab8fd344b864f597f33c8
Eduardo dos Santos Leal <eduardoleal1981@gmail.com>
2022-05-11 23:43:00 -0300

    Desafio 02 - Adicionando edição e alertas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,32 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
  
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+export interface TaskNewTitleProps {
+  taskId: number;
+  taskNewTitle: string
+}
+
 export function Home() {
+  
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    setTasks(oldTasks => [...oldTasks, {
-      id: new Date().getTime(),
-      title: newTaskTitle,
-      done: false
-    }]);
+    const elementoExistente = tasks.find(element => element.title === newTaskTitle);
+    if (elementoExistente === undefined){
+      setTasks(oldTasks => [...oldTasks, {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      }]);
+    } else {
+      Alert.alert("Task já cadastrada",
+      "Você não pode cadastrar uma task com o mesmo nome");
+    }
+
   }
 
   function handleToggleTaskDone(id: number) {
@@ -27,30 +40,57 @@ export function Home() {
     setTasks(updatedTasks);
   }
 
+  function handleRemoveTaskAlert(id: number) {
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: "Não",
+          onPress: () => console.log("Seleção de Não"),
+          style: "cancel"
+        },
+        { text: "Sim",
+          onPress:() => handleRemoveTask(id)
+        }
+      ]
+    );
+  }
+
   function handleRemoveTask(id: number) {
     const newTasksArray = tasks.filter(item => {
       return item.id != id;
     });
-    setTasks(newTasksArray);
+    setTasks(newTasksArray)
+  }
+
+  function handleEditTask(taskNewTitleProps: TaskNewTitleProps) {
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    updatedTasks.forEach(element => {
+      if (element.id === taskNewTitleProps.taskId){ 
+        element.title = taskNewTitleProps.taskNewTitle;
+        return;
+      }
+    })
+    setTasks(updatedTasks);
   }
 
   return (
-    <View style={styles.container}>
+    <View style={styles.containerHome}>
       <Header tasksCounter={tasks.length} />
-
       <TodoInput addTask={handleAddTask} />
-
-      <TasksList 
+      <TasksList
         tasks={tasks} 
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTaskAlert}
+        editTask={handleEditTask}
       />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerHome: {
     flex: 1,
     backgroundColor: '#EBEBEB'
   }
